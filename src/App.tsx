fb5797a69752f64b08dd879d392b42b20a54199d
Andrea Di Marco <andrea@dimarco.io>
2021-09-21 11:18:53 -0400

    chore: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <section className="container">
+      <header>
+        <img
+          src="/air-miles-logo.svg"
+          alt="Air Miles company logo"
+          className="logo"
+        />
+        <h1>Air Miles</h1>
+      </header>
+      <main>
+        <h2>Technical Interview</h2>
+      </main>
+    </section>
+  )
+}
+
+export default App
